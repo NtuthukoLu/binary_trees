2eea7ea8dbdec0847ce47800da17b14f518b7ac4
Ntuthuko <ntuthukolu@gmail.com>
2023-08-04 00:37:46 +0200

    depth func

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - gets the depth of a node in abinary tree
+ * @tree: root node to draw height from for tree
+ *
+ * Return: size_t representing height, 0 on failure or NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree || binary_tree_is_root(tree))
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
